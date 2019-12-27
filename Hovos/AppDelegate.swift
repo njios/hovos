ff7acb85b1002045f638c13c3cbe796e63c4ce04
neeraj <neeraj.joshi@mobileprogramming.net>
2019-12-27 23:21:30 +0530

    updated

--- Hovos/AppDelegate.swift
+++ Hovos/AppDelegate.swift
@@ -9,21 +9,28 @@
 import UIKit
 import GoogleSignIn
 import IQKeyboardManagerSwift
+import CoreLocation
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+var locationManager: CLLocationManager?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         IQKeyboardManager.shared.enable = true
         GIDSignIn.sharedInstance().clientID = "256440564385-h4159oo3clbprkg6t8f3btmdvi162l6p.apps.googleusercontent.com"
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestLocation()
+        locationManager?.startUpdatingLocation()
+      
         
         if let _ = UserDefaults.standard.value(forKey: constants.accessToken.rawValue){
             let storyboard = UIStoryboard(name: "Dashboard", bundle: nil)
              let vc = storyboard.instantiateViewController(withIdentifier: "tabvc")
             window?.rootViewController = vc
         }
+        
         return true
     }
     
@@ -31,5 +38,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return GIDSignIn.sharedInstance().handle(url)
     }
 
+}
+
+extension AppDelegate:CLLocationManagerDelegate{
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        manager.stopUpdatingLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+    }
 }
 
